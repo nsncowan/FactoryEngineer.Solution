2f7b9e35cae291bcfe40acb923d975e81e46eb24
Noah Cowan <nsncowan@gmail.com>
2023-03-24 10:09:57 -0700

    add "Required" data annotation

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "You must enter a name to add an engineer.")]
     public string Name { get; set; }
     public List<License> JoinEntities { get; }
   }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "You must enter a name to add a machine.")]
     public string Name { get; set; }
     public List<License> JoinEntities { get; }
   }
